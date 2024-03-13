0ad10dc92311d099d9d38ee2e3aba4813a3ae6dc
Paul Song <jaebinpsong@gmail.com>
2024-03-13 12:47:25 -0700

    Setter getter into Player factory

--- src/player.js
+++ src/player.js
@@ -1,6 +1,43 @@
 const Player = (() => {
-  const createPlayer = (playerName) => {
+  const createPlayer = (playerName, playerIdentity) => {
     let name = playerName
+    const identity = playerIdentity
+    let map
+    let searchQueue = []
+
+    function getName() {
+      return name
+    }
+
+    function getMap() {
+      return map
+    }
+
+    function getIdentity() {
+      return identity
+    }
+
+    // SETTERS
+
+    function setName(newName) {
+      name = newName
+    }
+
+    // CHECKERS
+
+    function isEmptyField(coordinate) {
+      const [x, y] = coordinate
+      return (
+        getMap().getBoard()[x][y] !== 'miss' &&
+        getMap().getBoard()[x][y] !== 'hit'
+      )
+    }
+
+    function isLoser() {
+      return getMap()
+        .getFleet()
+        .every((battleship) => battleship.getSunk() === true)
+    }
 
   }
 })()
